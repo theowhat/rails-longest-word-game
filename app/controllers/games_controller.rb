566d38acc1c88acbf4aebdd69983b1cc5641cd0e
theo watine <watinetheo@gmail.com>
2019-02-19 09:15:26 +0100

    ie 2 patially done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,20 @@
 class GamesController < ApplicationController
   def new
+    num = Random.new
+    @letters = ('A'..'Z').to_a.sample(num.rand(5..10))
   end
 
   def score
+    @word = params[:words]
+    @grid = params[:grid]
+    tests = @word.split('').map do |char|
+      @grid.include? char.capitalize
+    end.all?
+
+    if tests == false || @word.length > @grid.length
+      @answer = 'wrong letter(s) choosen or exceed word limit'
+    else
+      @answer = 'Correct word'
+    end
   end
 end
